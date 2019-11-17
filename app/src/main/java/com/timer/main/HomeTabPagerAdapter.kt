1de1972d88dc015958aac854ae158233b14ec18a
김지원 <kimji1@tmon.co.kr>
2019-11-17 15:40:15 +0900

    - change BottomNavigation > ViewPager & TabLayout

---
+++ app/src/main/java/com/timer/main/HomeTabPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.timer.main
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class HomeTabPagerAdapter(fm: FragmentManager?, private val fragments: List<Fragment>) :
+    FragmentPagerAdapter(fm) {
+    companion object {
+        const val HOME_TABS_COUNT = 3
+    }
+
+    override fun getItem(position: Int): Fragment = fragments[position]
+
+    override fun getItemPosition(`object`: Any): Int {
+        return fragments.indexOf(`object`)
+    }
+
+    override fun getCount(): Int {
+        return HOME_TABS_COUNT
+    }
+}
