8bc55113c7b7b18c3ab6b7e503b089bc581873ac
김지원 <kimji1@tmon.co.kr>
2019-11-18 09:52:40 +0900

    - change to fit into design guide for HOME

--- app/src/main/java/com/timer/home/HomeFragment.kt
+++ app/src/main/java/com/timer/home/HomeFragment.kt
@@ -1,44 +1,39 @@
 package com.timer.home
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.timer.R
 import com.timer.main.MainActivity
 import com.timer.toolbar.ToolbarFragment.Initializer
 import com.timer.toolbar.model.ToolbarButtonType
-import kotlinx.android.synthetic.main.fragment_home.view.*
-import org.jetbrains.anko.*
-import org.jetbrains.anko.sdk27.coroutines.onClick
-import org.jetbrains.anko.support.v4.toast
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
-    private val keys = arrayListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "", "0", "<", "시", "분", "초")
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false) as ViewGroup
-        view.container.gridLayout {
-            layoutParams = ViewGroup.LayoutParams(wrapContent, wrapContent)
-            columnCount = 3
-            keys.forEach { text ->
-                button(text) {
-                    tag = text
-                    width = dip(50)
-                    height = dip(65)
-                    textSize = 30F
-                    backgroundColor = Color.WHITE
-                    onClick { toast("clicked $text") }
-                }
-            }
-        }
-        return view
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initKeypad()
+        initTimerEditArea()
         initToolbar()
+    }
+
+    private fun initKeypad() {
+        val keypadElements =
+            listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "취소", "0", "<", "시", "분", "초")
+        keypadContainer.adapter = ArrayAdapter(context!!, R.layout.item_keypad, keypadElements)
+    }
+
+    private fun initTimerEditArea() {
+
     }
 
     private fun initToolbar() {
@@ -50,6 +45,5 @@ class HomeFragment : Fragment() {
             }.enableButton(ToolbarButtonType.Settings) {
                 // Settings button click listener
             })
-
     }
 }
