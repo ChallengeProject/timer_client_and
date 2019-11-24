e6a73a305b21f5b5c1d8c2cf2797459d90f25296
seoft <seoft.dev@gmail.com>
2019-11-24 23:17:45 +0900

    create empty project

---
+++ app/src/main/java/kr/co/seoft/two_min/MainActivity.kt
@@ -0,0 +1,19 @@
+package kr.co.seoft.two_min
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+
+
+}
