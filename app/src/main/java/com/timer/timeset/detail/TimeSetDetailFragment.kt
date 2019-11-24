887e708db7eff643f57ac315003b233da02a8a7c
seoft <seoft.dev@gmail.com>
2019-11-24 23:04:50 +0900

    init clear

--- app/src/main/java/com/timer/timeset/detail/TimeSetDetailFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.timer.timeset.detail
-
-import androidx.fragment.app.Fragment
-
-class TimeSetDetailFragment : Fragment() {
-}
--- app/src/main/java/com/timer/toolbar/ToolbarButtonType.kt
+++
@@ -1,5 +0,0 @@
-package com.timer.toolbar
-
-enum class ToolbarButtonType {
-    BACK, DELETE, SHARE, HOME, PAC_MAN, WISH, SETTINGS
-}
